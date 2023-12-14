d4f03b85e5ca98daf316ed422d977020c47d64dc
klimukviktori@gmail.com <klimukviktori@gmail.com>
2023-12-15 00:02:07 +0200

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const form = document.querySelector("form");
 //
 import { fetchHints } from "./js/pixabay";
 import{markupCard} from "./js/markup";
-import { lightBox } from "./js/simpleLightBox";
+import { lightBox } from "./js/lightBox";
 import {success, warning, info, error, loading, reLoading} from "./js/notiflix";
 
 const btnLoad = document.createElement("button");
