820c7786dfc722a33437f061f4cea144259bad8f
klimukviktori@gmail.com <klimukviktori@gmail.com>
2023-12-15 00:23:13 +0200

    commit

--- src/js/lightBox.js
+++ src/js/lightBox.js
@@ -2,7 +2,7 @@ import SimpleLightbox from "simplelightbox";
 
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-export let lightbox = new SimpleLightbox('.gallery a', {
+export const lightbox = new SimpleLightbox('.gallery a', {
     captionData: "alt",
     captionDelay: 300,
 });
