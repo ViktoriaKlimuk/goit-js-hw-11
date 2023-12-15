be6ef8871d5118a29339de5476a0f1b4c0d579a3
klimukviktori@gmail.com <klimukviktori@gmail.com>
2023-12-15 12:03:42 +0200

    12

--- src/js/markup.js
+++ src/js/markup.js
@@ -11,27 +11,25 @@ export function markupCard(params) {
             downloads 
         }) => `
         <div class="photo-card">
-            <a href="${largeImageURL}">
-                <img src="${webformatURL}" alt="${tags}" loading="lazy" width="310px"/>
-                <div class="info">
-                  <p class="info-item">
-                    <b>Likes</b><br>
-                    <b>${likes}</b>
-                  </p>
-                  <p class="info-item">
-                    <b>Views</b><br>
-                    <b>${views}</b>
-                  </p>
-                  <p class="info-item">
-                    <b>Comments</b><br>
-                    <b>${comments}</b>
-                  </p>
-                  <p class="info-item">
-                    <b>Downloads</b><br>
-                    <b>${downloads}</b>
-                  </p>
-                </div>
-            </a>
+          <img src="${webformatURL}" alt="${tags}" loading="lazy" width="310px"/>
+          <div class="info">
+            <p class="info-item">
+              <b>Likes</b><br>
+              <b>${likes}</b>
+            </p>
+            <p class="info-item">
+              <b>Views</b><br>
+              <b>${views}</b>
+            </p>
+            <p class="info-item">
+              <b>Comments</b><br>
+              <b>${comments}</b>
+            </p>
+            <p class="info-item">
+              <b>Downloads</b><br>
+              <b>${downloads}</b>
+            </p>
+          </div>
        </div>
         `
     ).join("");
